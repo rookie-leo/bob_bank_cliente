a0aa77f66f3a2e241955a8e41b37d538c81c35d4
rookie-leo <proleonardopa@gmail.com>
2024-09-06 09:42:48 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/bob/bank/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.bank.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
